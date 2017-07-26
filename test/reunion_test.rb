6c948b9cbb121f5e49e1c18e977f2ada5e5d3713
Becki Srofe <blf_23@yahoo.com>
2017-07-26 10:00:31 -0600

    Add reunion initial methods and tests

--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,24 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/emoji'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_it_exists
+    reunion = Reunion.new("Denver")
+
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_it_has_a_name
+    reunion = Reunion.new("Denver")
+
+    assert_equal "Denver", reunion.name
+  end
+
+  def test_it_has_no_activities_when_created
+  end
+
 end
