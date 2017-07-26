1c66d59e89a299140338ed33868eff59c8048a5c
Becki Srofe <blf_23@yahoo.com>
2017-07-26 09:16:13 -0600

    Add initial methods to activity

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -1,7 +1,33 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'minitest/emoji'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("Brunch")
+
+    assert_equal "Brunch", activity.name
+  end
+
+  def test_it_is_initialized_with_no_participants
+    activity = Activity.new("Brunch")
+
+    assert_equal ({}), activity.participants
+  end
+
+  def test_it_can_add_participants
+    activity = Activity.new("Brunch")
+    activity.add_participant("Jim", 20)
+
+    assert_equal ({"Jim" => 20}), activity.participants
+  end
 end
